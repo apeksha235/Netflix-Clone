530859bae91cb8f732500a21ba76e98d3b8e0bd2
hrishik18 <hrishik.s@somaiya.edu>
2022-11-30 23:03:51 +0530

    final commit with disabled errors

--- netflix-ui/src/pages/UserListedMovies.jsx
+++ netflix-ui/src/pages/UserListedMovies.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { onAuthStateChanged } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
